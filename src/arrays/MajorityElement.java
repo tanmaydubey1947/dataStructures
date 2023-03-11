bbf6fab36192eba629b32e5dd56e866ccbb9df39
TANMAY DUBEY <48459376+tanmaydubey1947@users.noreply.github.com>
2023-03-11 14:01:26 +0530

    Added file for majority element problem

---
+++ src/arrays/MajorityElement.java
@@ -0,0 +1,26 @@
+package arrays;
+
+public class MajorityElement {
+    /**
+     *
+     * Leetcode Link : <a href="https://leetcode.com/problems/majority-element/">...</a>
+     * <p>
+     * Iteration for better approach : 1
+     * <p>
+     * Approach:
+     * <p>
+     *     TODO
+     * <p>
+     *
+     *
+     */
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int majorityElement(int[] nums) {
+        return 0;
+    }
+
+}
