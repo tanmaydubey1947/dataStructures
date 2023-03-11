c7a934ee7b862ca8801d7289eb66041863b5d972
TANMAY DUBEY <48459376+tanmaydubey1947@users.noreply.github.com>
2023-03-11 14:39:39 +0530

    Added logic for MajorityElement problem

--- src/arrays/MajorityElement.java
+++ src/arrays/MajorityElement.java
@@ -1,26 +1,45 @@
 package arrays;
 
 public class MajorityElement {
-    /**
+     /**
      *
      * Leetcode Link : <a href="https://leetcode.com/problems/majority-element/">...</a>
      * <p>
      * Iteration for better approach : 1
      * <p>
-     * Approach:
+     * Approach: 1
      * <p>
-     *     TODO
+     *     Sort the array and then select the first element until the next value is encountered and then keep increasing
+     *     the count of that element. Now once the next element is encountered check if the count is greater that n/2.
+     *     If the count is greater than n/2 return the number else check for other numbers using same approach.
+     *     Time Complexity : O(n^2)
      * <p>
-     *
      *
      */
 
-    public static void main(String[] args) {
+     public static void main(String[] args) {
+
+        System.out.println(majorityElement(new int[]{-1, 1, 1, 1, 2, 1}));
 
     }
 
     public static int majorityElement(int[] nums) {
-        return 0;
+
+        Arrays.sort(nums);
+
+        int count = 1, i;
+
+        for (i = 0; i < nums.length - 1; i++) {
+
+            if (nums[i] == nums[i + 1]) {
+                count++;
+            } else if (count > nums.length / 2) {
+                return nums[i - 1];
+            } else {
+                count = 1;
+            }
+        }
+        return nums[i];
     }
 
 }
