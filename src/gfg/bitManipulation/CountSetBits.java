edd78d57bd0eb159de6cda5bb49dc9a8fda677b4
Tanmay Dubey <tanmaydubey1947@gmail.com>
2023-10-25 22:18:30 +0530

    Added countSetBits and powerOfTwo using bit manipulation

---
+++ src/gfg/bitManipulation/CountSetBits.java
@@ -0,0 +1,22 @@
+package gfg.bitManipulation;
+
+/**
+ *  Brian Kerningam's Algorithm: n = n & (n-1) => this will unset the last set bit
+ */
+public class CountSetBits {
+
+    public static void main(String[] args) {
+
+        System.out.println(countBits(4));
+
+    }
+
+    public static int countBits(int n) {
+        int res = 0;
+        while (n > 0) {
+            n = (n & (n - 1));
+            res++;
+        }
+        return res;
+    }
+}
