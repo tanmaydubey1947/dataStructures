478e5a46fee546c8a25e291b52aa043d81f777af
Tanmay Dubey <tanmaydubey1947@gmail.com>
2023-10-27 20:17:58 +0530

    Added sum of digits and palindrome

---
+++ src/gfg/recursion/lectureQuestions/Palindrome.java
@@ -0,0 +1,19 @@
+package gfg.recursion.lectureQuestions;
+
+public class Palindrome {
+
+    public static void main(String[] args) {
+
+        System.out.println(isPalindrome("abbaabba", 0, 7));
+    }
+
+    public static boolean isPalindrome(String str, int start, int end){
+
+        if(start>=end){
+            return true;
+        } else {
+            return (str.charAt(start) == str.charAt(end))
+                    && isPalindrome(str, start+1, end-1);
+        }
+    }
+}
