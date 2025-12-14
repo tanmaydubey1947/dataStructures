28bf21b0456852f6631a352a3bce077a87c0dfda
Tanmay Dubey <tanmaydubey1947@gmail.com>
2025-12-14 23:08:02 +0530

    #DP Frog Jump with K Distance Tabulation solution added

--- src/dynamicProgramming/medium/FrogJumpKDistance.java
+++ src/dynamicProgramming/medium/FrogJumpKDistance.java
@@ -4,13 +4,15 @@ import java.util.Arrays;
 
 public class FrogJumpKDistance {
 
-    public int frogJump(int[] heights, int k) {
+    //Memoization + Recursion
+
+    public int frogJumpMemoization(int[] heights, int k) {
         int[] dp = new int[heights.length];
         Arrays.fill(dp, -1);
-        return helper(heights, k, 0, dp);
+        return frogJumpMemoization(heights, k, 0, dp);
     }
 
-    private int helper(int[] heights, int k, int i, int[] dp) {
+    private int frogJumpMemoization(int[] heights, int k, int i, int[] dp) {
 
         if (i >= heights.length - 1) {
             return 0;
@@ -24,11 +26,39 @@ public class FrogJumpKDistance {
 
         for (int j = 1; j <= k; j++) {
             if (i + j < heights.length) {
-                int cur = helper(heights, k, j + i, dp) + Math.abs(heights[i] - heights[i + j]);
+                int cur = frogJumpMemoization(heights, k, j + i, dp) + Math.abs(heights[i] - heights[i + j]);
                 minStep = Math.min(cur, minStep);
             }
         }
 
         return dp[i] = minStep;
+    }
+
+
+    //Tabulation
+
+    public int frogJump(int[] heights, int k) {
+
+        int n = heights.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, -1);
+        dp[0] = 0;
+
+        for (int i = 1; i < n; i++) {
+
+            int minStep = Integer.MAX_VALUE;
+
+            for (int j = 1; j <= k; j++) {
+
+                if (i - j >= 0) {
+                    int cur = dp[i - 1] + Math.abs(heights[i] - heights[i - j]);
+                    minStep = Math.min(minStep, cur);
+                }
+            }
+
+            dp[i] = minStep;
+        }
+
+        return dp[n - 1];
     }
 }
