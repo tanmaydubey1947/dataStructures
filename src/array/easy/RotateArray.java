9504e3363cdb4becb996d7f1f933a3132bede4a9
Tanmay Dubey <tanmaydubey1947@gmail.com>
2025-12-16 21:50:30 +0530

    #Array RotateArray solution added

---
+++ src/array/easy/RotateArray.java
@@ -0,0 +1,24 @@
+package array.easy;
+
+public class RotateArray {
+
+    public void rotate(int[] nums, int k) {
+
+        int n = nums.length;
+        k = k % n;
+
+        reverse(0, n - k - 1, nums);
+        reverse(n - k, n - 1, nums);
+        reverse(0, n - 1, nums);
+    }
+
+    private void reverse(int start, int end, int[] arr) {
+        while (start <= end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
