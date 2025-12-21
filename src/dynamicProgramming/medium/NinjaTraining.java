b47703a97cd2878a316e4d0972c8aa7d1c0f600e
Tanmay Dubey <tanmaydubey1947@gmail.com>
2025-12-21 19:45:33 +0530

    #DP NinjaSolution solution added

---
+++ src/dynamicProgramming/medium/NinjaTraining.java
@@ -0,0 +1,84 @@
+package dynamicProgramming.medium;
+
+import java.util.Arrays;
+
+
+// Recursion + Memoization
+class RecursionWithMemoization {
+    public int ninjaTraining(int[][] matrix) {
+        int n = matrix.length;
+        int[][] dp = new int[n + 1][4];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+        return solve(3, matrix, 0, dp);
+    }
+
+    private int solve(int last, int[][] matrix, int i, int[][] dp) {
+
+        if (i == matrix.length - 1) {
+            int max = Integer.MIN_VALUE;
+
+            for (int j = 0; j < 3; j++) {
+
+                if (j != last) {
+                    max = Math.max(max, matrix[i][j]);
+                }
+            }
+
+            return max;
+        }
+
+        if (dp[i][last] != -1) {
+            return dp[i][last];
+        }
+
+        int max = Integer.MIN_VALUE;
+
+        for (int j = 0; j < 3; j++) {
+
+            if (j != last) {
+
+                int points = matrix[i][j] + solve(j, matrix, i + 1, dp);
+                max = Math.max(points, max);
+            }
+        }
+
+        return dp[i][last] = max;
+    }
+}
+
+// Tabulation
+
+
+class Tabulation {
+    public int ninjaTraining(int[][] matrix) {
+
+        int n = matrix.length;
+        int[][] dp = new int[n][4];
+
+        dp[0][0] = Math.max(matrix[0][1], matrix[0][2]);
+        dp[0][1] = Math.max(matrix[0][2], matrix[0][0]);
+        dp[0][2] = Math.max(matrix[0][1], matrix[0][0]);
+        dp[0][3] = Math.max(matrix[0][1], Math.max(matrix[0][2], matrix[0][0]));
+
+        for (int day = 1; day < n; day++) {
+
+            for (int last = 0; last < 4; last++) {
+                dp[day][last] = 0;
+
+                for (int task = 0; task <= 2; task++) {
+
+                    if (last != task) {
+
+                        int activity = matrix[day][task] + dp[day - 1][task];
+                        dp[day][last] = Math.max(activity, dp[day][last]);
+                    }
+                }
+            }
+        }
+
+        return dp[n - 1][3];
+    }
+}
+
