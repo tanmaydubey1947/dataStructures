88e36dac8bcb323ae3d90f923e74d173e9b8fb5d
Tanmay Dubey <tanmaydubey1947@gmail.com>
2026-01-20 09:46:21 +0530

    Proxy pattern code added

--- src/designPatterns/structural/Proxy.java
+++ src/designPatterns/structural/Proxy.java
@@ -1,4 +1,57 @@
 package designPatterns.structural;
 
+import java.util.*;
+
+interface VideoDownloader {
+    String downloadVideo(String videoURL);
+}
+
+// ========== RealVideoDownloader Class ==========
+class RealVideoDownloader implements VideoDownloader {
+
+    @Override
+    public String downloadVideo(String videoUrl) {
+        System.out.println("Downloading video from URL: " + videoUrl);
+        return "Video content from " + videoUrl;
+    }
+}
+
+// =============== Proxy With Cache ====================
+class CachedVideoDownloader implements VideoDownloader {
+
+    private RealVideoDownloader realDownloader;
+    private Map<String, String> cache;
+
+    public CachedVideoDownloader() {
+        this.realDownloader = new RealVideoDownloader();
+        this.cache = new HashMap<>();
+    }
+
+    @Override
+    public String downloadVideo(String videoUrl) {
+        if (cache.containsKey(videoUrl)) {
+            System.out.println("Returning cached video for: " + videoUrl);
+            return cache.get(videoUrl);
+        }
+
+        System.out.println("Cache miss. Downloading...");
+        String video = realDownloader.downloadVideo(videoUrl);
+        cache.put(videoUrl, video);
+        return video;
+    }
+}
+
+
+// ================ Main Class ===================
 public class Proxy {
+    public static void main(String[] args) {
+        VideoDownloader cacheVideoDownloader = new CachedVideoDownloader();
+        System.out.println("User 1 tries to download the video.");
+        cacheVideoDownloader.downloadVideo("https://video.com/proxy-pattern");
+
+        System.out.println();
+
+        System.out.println("User 2 tries to download the same video again.");
+        cacheVideoDownloader.downloadVideo("https://video.com/proxy-pattern");
+    }
 }
