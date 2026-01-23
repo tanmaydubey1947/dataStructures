340a284bb4dea50ba686b0273cdbf61a66fae080
Tanmay Dubey <tanmaydubey1947@gmail.com>
2026-01-23 18:28:05 +0530

    #Tree IsSameTree solution added

---
+++ src/binaryTree/easy/IsSameTree.java
@@ -0,0 +1,26 @@
+package binaryTree.easy;
+
+import binaryTree.TreeNode;
+
+public class IsSameTree {
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+
+        if (p == null && q == null) {
+            return true;
+        }
+
+        if (p == null || q == null) {
+            return false;
+        }
+
+        if (p.val != q.val) {
+            return false;
+        }
+
+        boolean left = isSameTree(p.left, q.left);
+        boolean right = isSameTree(p.right, q.right);
+
+        return left && right;
+    }
+}
