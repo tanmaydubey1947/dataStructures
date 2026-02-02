ee29a2b79f6ac5b4924f45cefa89aa1a7cd3f533
Tanmay Dubey <tanmaydubey1947@gmail.com>
2026-02-02 23:36:32 +0530

    #DP GridUniquePaths Tabulation solution added

--- src/dynamicProgramming/medium/GridUniquePaths.java
+++ src/dynamicProgramming/medium/GridUniquePaths.java
@@ -33,6 +33,39 @@ class GridUniquePaths {
 
         return dp[i][j] = right + bottom;
     }
+
+    // Tabulation
+
+    public int uniquePaths(int m, int n) {
+
+        int[][] dp = new int[m][n];
+        dp[m - 1][n - 1] = 1;
+        int ans = 0;
+
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+
+                if (i == m - 1 && j == n - 1) {
+                    continue;
+                } else {
+
+                    int right = 0;
+                    int down = 0;
+                    if (i + 1 <= m - 1) {
+                        right = dp[i + 1][j];
+                    }
+
+                    if (j + 1 <= n - 1) {
+                        down = dp[i][j + 1];
+                    }
+
+                    dp[i][j] = Math.max(ans, right + down);
+                }
+            }
+        }
+
+        return dp[0][0];
+    }
 }
 
 
