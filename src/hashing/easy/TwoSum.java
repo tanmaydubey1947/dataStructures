becca8042cddfe06a9ecc742e117cae330e72958
Tanmay Dubey <tanmaydubey1947@gmail.com>
2026-02-02 21:07:58 +0530

    #Hashing TwoSum solution added

---
+++ src/hashing/easy/TwoSum.java
@@ -0,0 +1,18 @@
+package hashing.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int val = target - nums[i];
+            if (map.containsKey(val)) {
+                return new int[]{i, map.get(val)};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{0, 0};
+    }
+}
