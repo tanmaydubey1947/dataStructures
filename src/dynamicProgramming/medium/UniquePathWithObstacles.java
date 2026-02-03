69a50b8e13f005eac53934f1aea6a25f5d1c5552
Tanmay Dubey <tanmaydubey1947@gmail.com>
2026-02-03 22:41:52 +0530

    #DP UniquePathWithObstacles Tabulation solution added

--- src/dynamicProgramming/medium/UniquePathWithObstacles.java
+++ src/dynamicProgramming/medium/UniquePathWithObstacles.java
@@ -30,5 +30,47 @@ class UniquePathWithObstacles {
 
         return dp[i][j] = right + down;
     }
+
+    // Tabulation
+    public int uniquePathsWithObstacles(int[][] matrix) {
+
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        if (matrix[0][0] == 1 || matrix[n - 1][m - 1] == 1) {
+            return 0;
+        }
+        int[][] dp = new int[n][m];
+
+        dp[n - 1][m - 1] = 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+                if (i == n - 1 && j == m - 1) {
+                    continue;
+                } else {
+
+                    if (matrix[i][j] == 1) {
+                        dp[i][j] = 0;
+                        continue;
+                    }
+
+                    int up = 0, left = 0;
+
+                    if (i + 1 <= n - 1) {
+                        up = dp[i + 1][j];
+                    }
+
+                    if (j + 1 <= m - 1) {
+                        left = dp[i][j + 1];
+                    }
+
+                    dp[i][j] = up + left;
+                }
+            }
+        }
+
+        return dp[0][0];
+    }
 }
 
