c29342087dd6c64c51545f282d976480f0e4ce2c
Tanmay Dubey <tanmaydubey1947@gmail.com>
2026-02-07 22:30:59 +0530

    #D{ MinimumFallingSum tabulation solution added

--- src/dynamicProgramming/medium/MinimumFallingSum.java
+++ src/dynamicProgramming/medium/MinimumFallingSum.java
@@ -70,4 +70,44 @@ public class MinimumFallingSum {
 
         return dp[i][j] = Math.min(bottom, Math.min(bottomLeft, bottomRight));
     }
+
+    // Tabulation
+
+    public int minFallingPathSumTabulation(int[][] matrix) {
+
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        int[][] dp = new int[n][m];
+        for (int j = 0; j < m; j++) {
+            dp[0][j] = matrix[0][j];
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+
+                int down = dp[i - 1][j] + matrix[i][j];
+                int left = (int) 1e9;
+                int right = (int) 1e9;
+
+                if (i - 1 >= 0 && j - 1 >= 0) {
+                    left = matrix[i][j] + dp[i - 1][j - 1];
+                }
+
+                if (j + 1 < m && i - 1 >= 0) {
+                    right = matrix[i][j] + dp[i - 1][j + 1];
+                }
+
+                dp[i][j] = Math.min(down, Math.min(left, right));
+            }
+        }
+
+        int ans = (int) 1e9;
+
+        for (int j = 0; j < m; j++) {
+            ans = Math.min(ans, dp[n - 1][j]);
+        }
+
+        return ans == (int) 1e9 ? -1 : ans;
+    }
 }
